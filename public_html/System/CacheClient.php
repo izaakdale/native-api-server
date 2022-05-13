64767788aa64f4ac7c7798de203c7cd28a0850b6
izaakdale <izaakdale@live.com>
2022-05-13 11:21:23 -0700

    Added redis cache client

---
+++ public_html/System/CacheClient.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace System;
+
+use Predis\Client as client;
+
+class CacheClient {
+
+    private $client = null;
+
+    public function __construct()
+    {
+        $host = getenv("CACHE_HOST");
+        $port = getenv('CACHE_PORT');
+
+        $this->client = new client([
+            'scheme' => 'tcp',
+            'host'   => $host,
+            'port'   => $port,
+        ]);
+    }
+
+    public function getClient(){
+        return $this->client;
+    }
+
+}
--- public_html/bootstrap.php
+++ public_html/bootstrap.php
@@ -2,8 +2,10 @@
 
 require 'vendor/autoload.php';
 require 'System/DatabaseConnector.php';
+require 'System/CacheClient.php';
 
 use Dotenv\Dotenv;
+use \System\CacheClient;
 use \System\DatabaseConnector;
 
 $dotenv = new Dotenv(__DIR__);
@@ -11,5 +13,7 @@ $dotenv->load();
 
 $db = new DatabaseConnector();
 $dbConnection = $db->getConnection();
+$client = new CacheClient();
+$cacheClient = $client->getClient();
 
 ?>
