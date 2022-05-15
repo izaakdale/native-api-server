1bda97ebf924c42546345f2b603659b07b594463
izaakdale <izaakdale@live.com>
2022-05-14 18:21:46 -0700

    Index.php tidy up

--- public_html/public/index.php
+++ public_html/public/index.php
@@ -15,10 +15,10 @@ $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $uri = explode('/', $uri);
 $requestMethod = $_SERVER["REQUEST_METHOD"];
 
-
+// cacheClient from bootstap
 if(authenticate($cacheClient))
 {
-    if($uri[1] == 'api')
+    if('api' == $uri[1])
     {
         // form of api request should be in {{URL}}/api/model/param, explode => [0]/[1]/[2]/[3]
         switch ($uri[2]) {
@@ -31,7 +31,6 @@ if(authenticate($cacheClient))
                 $productController->processRequest($requestMethod, isset($uri[3]) ? (int) $uri[3] : null);
                 break;
             default:
-                $controller = new Controller($dbConnection);
                 return Controller::notFoundResponse();
                 break;
         }
